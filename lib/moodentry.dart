61b592229659fa4a29247c6270816583be1761ab
Martin Hoffmann <maedhoff@ucsc.edu>
2019-02-08 19:57:06 -0800

    Updated generateListEntry() method to accurately reflect mood score

--- lib/moodentry.dart
+++ lib/moodentry.dart
@@ -5,7 +5,7 @@ class MoodEntry /*implements Comparable*/{
   DateTime _time;
   int _score;
   bool _isRegularEntry;
-  
+
   MoodEntry(DateTime time, int score, [bool isRegularEntry = true]){
     this._time = time;
     this._score = score;
@@ -37,16 +37,43 @@ class MoodEntry /*implements Comparable*/{
 }
 
 class MoodEntryDay {
+  
+  static List moods = [
+    {'label': 'Very Bad',
+      'icon': Icons.sentiment_very_dissatisfied,
+      'color': Colors.indigo[900],
+    },
+    {'label': 'Bad',
+      'icon': Icons.sentiment_dissatisfied,
+      'color': Colors.blue,
+    },
+    {'label': 'Moderate',
+      'icon': Icons.face,
+      'color': Colors.grey,
+    },
+    {'label': 'Good',
+      'icon': Icons.sentiment_satisfied,
+      'color': Colors.green[600],
+    },
+    {'label': 'Very Good',
+      'icon': Icons.sentiment_very_satisfied,
+      'color': Colors.green[400],
+    },
+  ];
+
   DateTime _day;
-  List<MoodEntry> _entries;
+  List _entries;
 
   MoodEntryDay(DateTime day){
     this._day = day;
+    this._entries = [];
     //print("A new moodentryday was generated on $day");
   }
 
   void addMoodEntry(DateTime time, int score){
-
+    this._entries.add(
+      new MoodEntry(time, score)
+    );
   }
 
   Widget generateListEntry(){
@@ -63,58 +90,52 @@ class MoodEntryDay {
       label = 'Today';
     }
 
+    List<Widget> rowChildren = [
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(bottom: 4),
+              child: Text(
+                label,
+                style: TextStyle(
+                  fontSize: 18,
+                  //either a Sunday or todays date
+                  fontWeight: labelWeight,
+                ),
+              ),
+            ),
+            Text(
+              subLabel,
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                color: Colors.grey[500]
+              ),
+            ),
+          ],
+        ),
+      ),
+    ];
+    print(this._entries.length);
+    for(int i=0, score; i<3; i++){
+      score = this._entries[i].score;
+      Container iconContainer = Container(
+        padding: EdgeInsets.only(left: 5),
+        child: Icon(
+          moods[score]['icon'],
+          color: moods[score]['color'],
+        ),
+      );
+
+      rowChildren.add(iconContainer);
+    };
+    
+
     return Container(
       padding: const EdgeInsets.only(top: 5, bottom: 5),
       child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(bottom: 4),
-                  child: Text(
-                    label,
-                    style: TextStyle(
-                      fontSize: 18,
-                      //either a Sunday or todays date
-                      fontWeight: labelWeight,
-                    ),
-                  ),
-                ),
-                Text(
-                  subLabel,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    color: Colors.grey[500]
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          Container(
-            padding: EdgeInsets.only(left: 5),
-            child: Icon(
-              Icons.sentiment_very_satisfied,
-              color: Colors.yellow[700],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 5),
-            child: Icon(
-              Icons.sentiment_satisfied,
-              color: Colors.green[600],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 5),
-            child: Icon(
-              Icons.sentiment_very_dissatisfied,
-              color: Colors.indigo[900],
-            ),
-          ),          
-        ],
+        children: rowChildren,
       ),
     );
   }
